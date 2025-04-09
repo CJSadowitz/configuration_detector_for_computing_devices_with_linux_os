2a10b5b1b93f50dccc1254dff13cb660ce168f53
colin sadowitz <sadowitzc@gmail.com>
2025-04-09 14:01:41 -0400

    Argument Class setup

--- src/ArgumentParser.py
+++ src/ArgumentParser.py
@@ -1,7 +1,20 @@
+import argparse
+
 class ArgumentParser():
 	def __init__(self):
 		# Setup all arguments
-		cmd_arguments = []
+		parser = argparse.ArgumentParser(description="Uh oh")
+		parser.add_argument("--get_packages", action="store_true", help="returns a list of all installed software")
+		parser.add_argument("--get_CPU_info", action="store_true", help="returns CPU info")
+		parser.add_argument("--get_mem_info", action="store_true", help="returns information on memory")
+		parser.add_argument("--get_devices",  action="store_true", help="returns a list of connected peripherals")
+		parser.add_argument("--get_linux_ver",  action="store_true", help="returns linux version")
+
+		self.cmd_arguments = parser.parse_args()
 
 	def get_cmd_arguments(self):
-		pass
+		return self.cmd_arguments
+
+if __name__ == "__main__":
+	asdf = ArgumentParser()
+	print (asdf.get_cmd_arguments())
--- test/ArgumentParserTest.py
+++ test/ArgumentParserTest.py
@@ -5,5 +5,18 @@ sys.path.append("..")
 from src.ArgumentParser import ArgumentParser
 
 class ArgumentParserTest(unittest.TestCase):
-	def test_get_cmd_arguments(self):
-		pass
+	def test_get_cmd_arguments_get_packages(self):
+		test_args = ['n/a', '--get_packages']
+		sys.argv = test_args
+
+		argument_parser_obj = ArgumentParser()
+		args = argument_parser_obj.get_cmd_arguments()
+
+		self.assertTrue(args.get_packages)
+		self.assertFalse(args.get_CPU_info)
+		self.assertFalse(args.get_mem_info)
+		self.assertFalse(args.get_devices)
+		self.assertFalse(args.get_linux_ver)
+
+if __name__ == "__main__":
+	unittest.main()
