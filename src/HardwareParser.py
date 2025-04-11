a91907efb5f9de214b876b3fede106d3df7d043c
Norberttony <norberttony@icloud.com>
2025-04-11 12:35:00 -0400

    get_mem_info() implementation

--- src/HardwareParser.py
+++ src/HardwareParser.py
@@ -1,12 +1,37 @@
+import subprocess
+
 class HardwareParser():
 	def __init__(self):
-		pass
+		self.lshw_output = False
+		try:
+			# initialize by running the lshw command
+			proc = subprocess.run([ "lshw", "-short" ], capture_output=True)
+			out = proc.stdout.decode()
+			# ignore table header, separator, and the last line (which is empty)
+			lines = out.split("\n")[2:-1]
+			self.lshw_output = lines
+		except FileNotFoundError:
+			print("ERROR: Could not find lshw")
+		except Exception as e:
+			print(f"ERROR: Unexpected error occurred when running lshw: {e}")
 
 	def get_CPU_info(self):
 		pass
 
 	def get_mem_info(self):
-		pass
+		if not self.lshw_output:
+			print("ERROR: Failed to fetch memory info")
+			return []
+		memory = []
+		for line in self.lshw_output:
+			# [0] has HW path
+			# [1] has class (should be memory)
+			# [2:] has description
+			elems = line.split()
+			if elems[1] == "memory":
+				desc = " ".join(elems[2:])
+				memory.append(desc)
+		return memory
 
 	def get_devices(self):
 		pass
