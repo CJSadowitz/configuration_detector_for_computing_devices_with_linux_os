5dded5368453ca634f4fd8ab9d13adf7b839f0d9
colin sadowitz <sadowitzc@gmail.com>
2025-04-17 14:52:43 -0400

    save packages to class member

--- src/PacManInterface.py
+++ src/PacManInterface.py
@@ -5,7 +5,7 @@ class PacManInterface:
 		self.packages = False
 
 	def run_command(self, cmd):
-		if cmd != "pacman -Q" and "apt list --installed | awk -F'[ /]' '{print $1, $3}'":
+		if cmd != "pacman -Q" and cmd != "apt list --installed | awk -F'[ /]' '{print $1, $3}'":
 			return []
 		with os.popen(cmd) as stdout:
 			return stdout.read().strip()
@@ -15,9 +15,12 @@ class PacManInterface:
 			with open("/etc/os-release") as os_release:
 				os_info = os_release.read().lower()
 			if "arch" in os_info:
-				return self.run_command("pacman -Q")
+				self.packages = self.run_command("pacman -Q")
+				return self.packages
 			elif "ubuntu" in os_info or "debian" in os_info:
-				return self.run_command("apt list --installed | awk -F'[ /]' '{print $1, $3}'")
+				self.packages = self.run_command("apt list --installed | awk -F'[ /]' '{print $1, $3}'")
+				return self.packages
 			# and so on
 		except FileNotFoundError:
-			return "OS not identifiable."
+			self.packages = "OS not identifiable."
+			return self.packages
